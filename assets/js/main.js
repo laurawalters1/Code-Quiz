7c6e3177a674eb3cd823bcd72b30d0225c5d6b41
Laura Walters <laura.walters1098@yahoo.com>
2022-01-08 20:00:08 +0000

    beginning to add checkbox functionality

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,13 +3,17 @@ var startbtn = document.getElementById("startbtn");
 var timer = document.getElementById("timer");
 var question = document.getElementById("question");
 var checkbox1 = document.getElementById("option1");
+var checkbox2 = document.getElementById("option2");
+var checkbox3 = document.getElementById("option3");
+var checkbox4 = document.getElementById("option4");
 var answer1 = document.getElementById("answer1");
 var answer2 = document.getElementById("answer2");
 var answer3 = document.getElementById("answer3");
 var answer4 = document.getElementById("answer4");
 var checkboxes = document.querySelectorAll(".checkbox");
+var score = document.getElementById("score");
 startbtn.addEventListener("click", function () {
-  timer.textContent = 3;
+  timer.textContent = 60;
   var timerInterval = setInterval(function () {
     timer.textContent -= 1;
     if (timer.textContent == 0) {
@@ -28,4 +32,28 @@ startbtn.addEventListener("click", function () {
   for (i = 0; i < checkboxes.length; i++) {
     checkboxes[i].style.display = "inline";
   }
+});
+
+document.addEventListener("click", function () {
+  //for (i = 0; i < checkboxes.length; i++) {
+  //   if (checkboxes[i].checked) {
+  //     console.log(checkboxes[i]);
+  //   }
+  if (
+    checkboxes[1].checked &&
+    !checkboxes[0].checked &&
+    !checkboxes[2].checked &&
+    !checkboxes[3].checked
+  ) {
+    console.log("correct");
+    score.textContent += 1;
+  } else if (
+    checkboxes[0].checked ||
+    checkboxes[2].checked ||
+    checkboxes[3].checked
+  ) {
+    console.log("incorrect");
+    timer.textContent -= 5;
+  }
+  //  }
 });
