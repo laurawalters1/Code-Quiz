a18d099ef87f45281e8d5068300c3d7525e8ba64
Laura Walters <laura.walters1098@yahoo.com>
2022-01-08 18:06:08 +0000

    setting start screen elements to display: none

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,15 @@
+var title = document.getElementById("title");
 var startbtn = document.getElementById("startbtn");
 var timer = document.getElementById("timer");
 startbtn.addEventListener("click", function () {
-  timer.textContent = 5;
+  timer.textContent = 60;
   var timerInterval = setInterval(function () {
     timer.textContent -= 1;
     if (timer.textContent == 0) {
       clearInterval(timerInterval);
     }
   }, 1000);
+
+  title.style.display = "none";
+  startbtn.style.display = "none";
 });
