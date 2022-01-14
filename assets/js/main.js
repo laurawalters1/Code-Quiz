1e7d9b2c15d70480d4f44c40cb877e353d3aaf99
Laura Walters <laura.walters1098@yahoo.com>
2022-01-14 18:02:15 +0000

    creating round one

--- assets/js/main.js
+++ assets/js/main.js
@@ -64,6 +64,57 @@ var finalScore = document.getElementById("final-score");
 var scoreArray = [];
 var clearScores = document.getElementById("clearScores");
 
+// First round function
+function firstRound() {
+  // For loop causes one button per option from the questions.[i].options array to be created, and assigns each of them a class from the questions[i].classes array
+  // this means that the correct answer can be distinguished from the others, these classes will be swapped out in the coming rounds
+  for (var i = 0; i < question1.options.length; i++) {
+    var listItem = document.createElement("button");
+    listItem.textContent = question1.options[i];
+    list.appendChild(listItem);
+    questionText.textContent = question1.questionContent;
+    listItem.classList.add("button");
+    listItem.classList.add("question-button");
+    listItem.classList.add(question1.classes[i]);
+    listItem.value = question1.classes[i];
+    optionArray.push(listItem);
+    // The below event listener runs a function that will test if the button the user clicked on contains the class of incorrect or correct, and proceed accordingly
+
+    optionArray[i].addEventListener("click", function ifCorrect(event) {
+      if (event.currentTarget.classList.contains("incorrect.1")) {
+        // If the user is incorrect, the timer will deduct 5 seconds, the screen will turn red after 1ms and will be set back to normal after 200ms
+        timerDisplay.textContent -= 5;
+
+        var incorrectAnswer = setInterval(function () {
+          document.body.style.backgroundColor = "rgb(253, 87, 87)";
+        }, 1);
+        var clearIncorrect = setInterval(function () {
+          document.body.style.backgroundColor = "rgb(141, 175, 247)";
+          clearInterval(incorrectAnswer);
+          clearInterval(clearIncorrect);
+        }, 200);
+        // Then the second round function will be ran
+        secondRound();
+      } else if (event.currentTarget.classList.contains("correct.1")) {
+        // If the user is correct, the value of score will increase by one and the text content of score display will update in accordance with this
+        score += 1;
+        scoreDisplay.textContent = "Score: " + score;
+        // The background will change to green after 1ms and change back after 200ms
+        var correctAnswer = setInterval(function () {
+          document.body.style.backgroundColor = "rgb(50, 205, 109)";
+        }, 1);
+        var clearCorrect = setInterval(function () {
+          document.body.style.backgroundColor = "rgb(141, 175, 247)";
+          clearInterval(correctAnswer);
+          clearInterval(clearCorrect);
+        }, 200);
+
+        secondRound();
+      }
+    });
+  }
+}
+
 // When the user hits the start button, the header and start button will disappear, the start timer and first round functions will be ran
 startBtn.addEventListener("click", function () {
   startBtn.style.display = "none";
